b774c59f54482b60d74cf05b451b21451a38cb1a
CoderNandan <nandanindia1947@gmail.com>
2024-05-13 21:37:42 +0530

    resend domin add

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -14,7 +14,7 @@ export async function sendVerificationEmail({
 }): Promise<ApiResponse> {
   try {
     const data = await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "noreply@nandan.in.net",
       to: email,
       subject: "Mystey message | verification code ",
       react: VerificationEmail({ username, otp: verifyCode }),
